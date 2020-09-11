8caf670ab554450ea6418361c093cbc5cf443557
marcsello <punkosdmarcell@rocketmail.com>
2020-09-12 00:57:57 +0200

    Added google maps

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,25 +14,41 @@
     <b-row>
       <b-col class="my-3">
         <h2>Kedves Játékos!</h2>
-        <p>Köszöntelek a 2020-as Hunbrony által szervezett Magyar Brony Pikniken! A teljes fejlesztő csapat nevében pedig engedd meg, hogy köszönetet mondjak amiért részt veszel a játékunkban. Sokat dolgoztunk vele és reméljük, hogy jól fogsz szórakozni.</p>
+        <p>Köszöntelek a 2020-as Hunbrony által szervezett Magyar Brony Pikniken! A teljes fejlesztő csapat nevében
+          pedig engedd meg, hogy köszönetet mondjak amiért részt veszel a játékunkban. Sokat dolgoztunk vele és
+          reméljük, hogy jól fogsz szórakozni.</p>
         <b-img src="@/assets/derpyflag.png" fluid-grow class="my-3"/>
         <h3>Na de hogyan is kell ezt játszani?</h3>
         <p>Ha részt vettél a 2019-es Brony Pikniken már ismerős lehet a koncepció.</p>
         <p>A Margit szigeten a Piknik helyszínének közelében elrejtettünk számos kártyát rajtuk egy QR kóddal.</p>
-        <b-img src="@/assets/qrsample.png" fluid-grow />
-        <p>A feladatod az lesz, hogy ezeket a kártyákat megtaláld és a telefonod segítségével a rajtuk lévő kódokat beolvasd.</p>
-        <p>Ekkor a telefonod egy pónit fog neked megjeleníteni aki nagyon boldog, hogy egy ölelést oszthat meg veled. (Vagy nem. Lehet, hogy rossz napja volt)</p>
-        <p>Feltűnhet a fenti sötét sávban egy kis trófea ikon mellette egy számmal. Ez a versenyben vezető játékos által megölelt pónik számát mutatja.</p>
+        <p>Az alábbi térkép mutatja hogy mekkora területet érdemes átvizsgálni. Nincs semmi trükk, a határokon kívül tényleg nem rejtettünk el semmit.</p>
+        <b-aspect aspect="2:3" class="my-2">
+          <iframe class="fill" src="https://www.google.com/maps/d/u/0/embed?mid=1-jjj1c22H3qa3Y7a1XLfTCfjIcQBO0YD">
+          </iframe>
+        </b-aspect>
+        <p>A feladatod az lesz, hogy ezeket a kártyákat megtaláld és a telefonod segítségével a rajtuk lévő kódokat
+          beolvasd.</p>
+        <b-img src="@/assets/qrsample.png" fluid-grow/>
+        <p>Ekkor a telefonod egy pónit fog neked megjeleníteni aki nagyon boldog, hogy egy ölelést oszthat meg veled.
+          (Vagy nem. Lehet, hogy rossz napja volt)</p>
+        <p>Feltűnhet a fenti sötét sávban egy kis trófea ikon mellette egy számmal. Ez a versenyben vezető játékos által
+          megölelt pónik számát mutatja.</p>
         <h3>Verseny?</h3>
         <p>Bizony!</p>
-        <p>A játék egy verseny amit a legtöbb pónit megölelő játékos nyer a legrövidebb időn belül. A helyezetteket a játékidő lejárta után fogjuk kihirdetni!</p>
+        <p>A játék egy verseny amit a legtöbb pónit megölelő játékos nyer a legrövidebb időn belül. A helyezetteket a
+          játékidő lejárta után fogjuk kihirdetni!</p>
         <p>A játék 2020. Szeptember 12-én 10:00 órától 17:00 óráig tart.</p>
-        <p>Tartsd nyitva a szemed hogy te öleld meg a legtöbb pónit mert értékes nyeremények várnak! Ennek ellenére nyugodtan menj keresgélni a barátaiddal. Egyedül nyerni magányos dolog.</p>
+        <p>Tartsd nyitva a szemed hogy te öleld meg a legtöbb pónit mert értékes nyeremények várnak! Ennek ellenére
+          nyugodtan menj keresgélni a barátaiddal. Egyedül nyerni magányos dolog.</p>
         <h3>Mire van szükséged?</h3>
-        <p>Egy akármilyen okostelefonra, azon egy böngészőre és bármilyen QR kód olvasóra ami képes linkeket megnyitni.</p>
-        <p>Ha valamilyen oknál fogva nem tudsz QR kódot beolvasni az "Ölelés" oldalon manuálisan is beírhatod a kártyán található "kulcsot".</p>
-        <p><b>FONTOS:</b> Minden kártyát könnyen elérhető helyre rejtettünk. Egyikhez sem kell kúszni. mászni vagy magas épületekről szénakupacba ugrani. A játékban mindenki a saját felelősségére vesz részt! De kérünk mindenkit, hogy vigyázzon a saját és mások épségére!</p>
-        <b-img src="@/assets/detectiveponk.png" fluid-grow />
+        <p>Egy akármilyen okostelefonra, azon egy böngészőre és bármilyen QR kód olvasóra ami képes linkeket
+          megnyitni.</p>
+        <p>Ha valamilyen oknál fogva nem tudsz QR kódot beolvasni az "Ölelés" oldalon manuálisan is beírhatod a kártyán
+          található "kulcsot".</p>
+        <p><b>FONTOS:</b> Minden kártyát könnyen elérhető helyre rejtettünk. Egyikhez sem kell kúszni. mászni vagy magas
+          épületekről szénakupacba ugrani. A játékban mindenki a saját felelősségére vesz részt! De kérünk mindenkit,
+          hogy vigyázzon a saját és mások épségére!</p>
+        <b-img src="@/assets/detectiveponk.png" fluid-grow/>
         <p>Most, hogy mindent tudsz amit tudni lehet, menj és ölelj meg pár pónit.</p>
         <p>Jó szórakozást kívánunk és érezd jól magad a pikniken.</p>
         <p class="blockquote-footer">A fejlesztő csapat</p>
@@ -55,7 +71,12 @@ export default {
 </script>
 
 <style scoped>
-  h3 {
-    font-size: 1.45rem;
-  }
+h3 {
+  font-size: 1.45rem;
+}
+
+iframe.fill {
+  width: 100%;
+  height: 100%;
+}
 </style>
