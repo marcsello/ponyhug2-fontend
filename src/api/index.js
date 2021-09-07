b31411f450a96beddbe647567f91b443db3fb856
marcsello <punkosdmarcell@rocketmail.com>
2021-09-07 15:02:51 +0200

    Made API location environment dependant

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = "https://hugs.hunbrony.hu/api/"
+const API_BASE_URL = process.env.VUE_APP_API_LOCATION
 const LOCAL_STORAGE_KEY = "JWT"
 
 const COMMON_ERROR_CODES = {
