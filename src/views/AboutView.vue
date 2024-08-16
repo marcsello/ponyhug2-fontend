d3cea2b6f49fdf9f9fb737b11bba0848091a1fd0
Temlin Olivér <git@teml.in>
2024-08-16 18:03:49 +0200

    fixes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -21,7 +21,7 @@
         <b-card title="Tormakris" img-src="@/assets/tormakris.png" img-right class="my-2">
           <span class="text-muted">DevOps, Backend</span>
         </b-card>
-        <b-card title="oliva" img-src="@/assets/oliva.png" img-right class="my-2">
+        <b-card title="oliva" img-src="@/assets/oliva.png" img-left class="my-2">
           <span class="text-muted">Content</span>
         </b-card>
       </b-col>
